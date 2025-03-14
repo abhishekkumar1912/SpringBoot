0654e32e58a01664919e55bb5d63965867e2ed09
abhishek kumar <as2952352@gmail.com>
2025-03-14 14:45:58 +0000

    again doing commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,32 @@
 package org.example;
 
-import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Path;
 
 import java.util.List;
-//import okhttp3.OkHttp;
-//import okhttp3.OkHttpClient;
-//import okhttp3.Request;
-//import okhttp3.Response;
+import okhttp3.OkHttp;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class Main {
     public static void main(String[] args) {
-//        OkHttpClient   client=new OkHttpClient();
-// String url="https://jsonplaceholder.typicode.com/todos/1";
-//        Request request=new Request.Builder().url(url).build();
-//
-//        try(  Response response= client.newCall(request).execute()){
-//
-//             if(!response.isSuccessful()){
-//                 System.out.println("error" );
-//             }
-//            System.out.println("prnthing something");
-//            System.out.println(response.body().string());
-//
-//
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        System.out.println("Hello world!");
-//
+        OkHttpClient   client=new OkHttpClient();
+ String url="https://jsonplaceholder.typicode.com/todos/1";
+        Request request=new Request.Builder().url(url).build();
+
+        try(  Response response= client.newCall(request).execute()){
+
+             if(!response.isSuccessful()){
+                 System.out.println("error" );
+             }
+            System.out.println("prnthing something");
+            System.out.println(response.body().string());
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("Hello world!");
+
 
 
 
